5112fa2354e32c035c036ea242f8b5388317a95a
matltgr <mathis.laurentgucker@gmail.com>
2020-10-24 20:23:04 +0200

    Completed Assignement 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function is similar to makeVector
+## It enables to get/set a matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invMatrix <- NULL
+  set <- function(y){
+    x <<- y 
+    invMatrix <<- NULL
+  }
+  get <- function() x
+  setInv <- function(mat) invMatrix <<- solve(mat)
+  getInv <- function() invMatrix
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve enables to retrieve 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Checks for cache data
+        inv <- x$getInv()
+        if(!is.null(inv)){
+          message("data was cached")
+          return(inv)
+        }
+        ## Else compute and stores inverse
+        message("No cached data found, result will be stored")
+        mat <- x$get()
+        x$setInv(mat)
+        x$getInv()
 }
